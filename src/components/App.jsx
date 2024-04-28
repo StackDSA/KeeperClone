2aa19e84f54f082ad8061703da992d0285bcc5d1
Ishan Deshpande <ishangreat01@gmail.com>
2024-04-28 23:09:53 +0530

    Added notes through iteration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-
+import notes from '../notes';
 import './App.css'
 import Heading from './Heading'
 import Note from './Note'
@@ -6,7 +6,11 @@ import Footer from './Footer'
 function App() {
 
   return (
-    <div><Heading /><Note /> <Footer /></div>
+    <div><Heading />
+      {notes.map((note)=>{
+        return <Note key={note.key} title={note.title} content={note.content}/>
+      })}
+    <Footer /></div>
   )
 }
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,11 @@
-function Note(){
-    return <div>
-        <h1></h1>
-        <p></p>
-    </div>
+/* eslint-disable react/prop-types */
+function Note(props){
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
+        )
 }
+
 export default Note
